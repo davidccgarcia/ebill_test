b165ccae9da54264cc37a250ec6d9dda74cdf544
David García <ccristhiangarcia@gmail.com>
2018-09-20 11:22:43 -0500

    Add test rollback

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel - deploy
+                    Laravel - deploy and rollback en <a href="https://styde.net">Styde</a>
                 </div>
 
                 <div class="links">
